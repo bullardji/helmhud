f67e4bb9f418bd154749d0f4b3e725661dc6ea86
bullardji <102577354+bullardji@users.noreply.github.com>
2025-06-07 14:51:58 -0400

    Add LLM mention chat

--- guardian/__init__.py
+++ guardian/__init__.py
@@ -2,5 +2,6 @@ from .bot import bot
 from . import utils  # noqa: F401
 from . import events  # noqa: F401
 from . import commands  # noqa: F401
+from . import llm  # noqa: F401
 
 __all__ = ["bot"]
